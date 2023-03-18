0204ce68f1a3148ac3df090b10cb0820f8d15050
Vladimir Orlov <satancrew.true@gmail.com>
2023-03-18 23:25:34 +0300

    refactor: delete comments

--- rs-react/src/components/Search/Search.test.tsx
+++ rs-react/src/components/Search/Search.test.tsx
@@ -8,33 +8,24 @@ describe('Search />', () => {
     render(<Search />);
     const input = document.querySelector('input') as HTMLInputElement;
 
-    // input exists in the form component
     expect(input).toBeTruthy();
 
-    // is empty
     expect(input?.textContent).toBe('');
 
     if (input) {
-      // test the input text
       input.textContent = 'Something';
       expect(input.textContent).toBe('Something');
 
-      // test the type prop
       expect(input.type).toBe('text');
 
-      // test the name prop
       expect(input.name).toBe('text');
 
-      // test the value prop
       fireEvent.change(input, {
         target: {
           value: '',
         },
       });
       expect(input.value).toBe('');
-
-      // test the required prop with the jest-dom
-      // expect(input).toBeRequired();
     }
   });
 });
