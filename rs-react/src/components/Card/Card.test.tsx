41f47ff5a1f7f2a1063d9c43551be951d23bec5d
Vladimir Orlov <satancrew.true@gmail.com>
2023-03-19 22:53:03 +0300

    refactor: delete import from Card test

--- rs-react/src/components/Card/Card.test.tsx
+++ rs-react/src/components/Card/Card.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
 import Card from './Card';
 import { testData } from '../../common/data';
