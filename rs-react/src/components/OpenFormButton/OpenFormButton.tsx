08455c9c6a5e08e86e7c5c8221c7d248667ddd5c
Vladimir Orlov <satancrew.true@gmail.com>
2023-03-21 12:51:57 +0300

    feat: add open form button

---
+++ rs-react/src/components/OpenFormButton/OpenFormButton.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './OpenFormButton.scss';
+
+export class OpenFormButton extends Component {
+  render() {
+    return (
+      <button className="form-btn" type="button">
+        Add card
+      </button>
+    );
+  }
+}
--- rs-react/src/pages/HomePage/HomePage.tsx
+++ rs-react/src/pages/HomePage/HomePage.tsx
@@ -5,6 +5,7 @@ import './HomePage.scss';
 import { IProduct } from '../../common/interface';
 import { Card } from '../../components/Card/Card';
 import { mockData } from '../../common/data';
+import { OpenFormButton } from '../../components/OpenFormButton/OpenFormButton';
 
 export class HomePage extends Component {
   state = {
@@ -23,17 +24,20 @@ export class HomePage extends Component {
       <>
         <Header checkHomeBtn={true} checkAboutBtn={false} />
         <main className="main">
-          {this.state.products.map((el: IProduct) => (
-            <Card
-              key={el.id}
-              id={el.id}
-              title={el.title}
-              price={el.price}
-              image={el.image}
-              rating={el.rating}
-              category={el.category}
-            />
-          ))}
+          <OpenFormButton />
+          <div className="main__wrapper">
+            {this.state.products.map((el: IProduct) => (
+              <Card
+                key={el.id}
+                id={el.id}
+                title={el.title}
+                price={el.price}
+                image={el.image}
+                rating={el.rating}
+                category={el.category}
+              />
+            ))}
+          </div>
         </main>
       </>
     );
