9ee2222248b973d70027c52b4d202392a79e26ac
Vladimir Orlov <satancrew.true@gmail.com>
2023-04-02 13:58:52 +0300

    refactor: change App class to function component

--- rs-react/src/App.tsx
+++ rs-react/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage/HomePage';
 import { ErrorPage } from './pages/ErrorPage/ErrorPage';
@@ -6,15 +6,13 @@ import { AboutUsPage } from './pages/AboutUsPage/AboutUsPage';
 import { FormPage } from './pages/FormPage/FormPage';
 import './App.scss';
 
-export class App extends Component {
-  render() {
-    return (
-      <Routes>
-        <Route path="/" element={<HomePage products={[]} />} />
-        <Route path="/about" element={<AboutUsPage />} />
-        <Route path="/form" element={<FormPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    );
-  }
-}
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutUsPage />} />
+      <Route path="/form" element={<FormPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
+};
