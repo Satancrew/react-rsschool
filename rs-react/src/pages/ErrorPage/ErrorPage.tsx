924c28f78429cb9f65a9e806c4ba49bc7dd7c134
Vladimir Orlov <satancrew.true@gmail.com>
2023-04-04 12:45:37 +0300

    refactor: delete return from arrow function

--- rs-react/src/pages/ErrorPage/ErrorPage.tsx
+++ rs-react/src/pages/ErrorPage/ErrorPage.tsx
@@ -3,16 +3,14 @@ import { Link } from 'react-router-dom';
 import './ErrorPage.scss';
 
 export const ErrorPage = () => {
-  return (
-    <section className="error-page">
-      <div className="error-page__content">
-        <h2 className="error-page__title"> 404 Error </h2>
-        <h3>Page not found</h3>
-        <p className="error-page__text"> Sorry, this page isn`t available </p>
-        <Link to="/" className="error-page__link">
-          Back to home
-        </Link>
-      </div>
-    </section>
-  );
+  <section className="error-page">
+    <div className="error-page__content">
+      <h2 className="error-page__title"> 404 Error </h2>
+      <h3>Page not found</h3>
+      <p className="error-page__text"> Sorry, this page isn`t available </p>
+      <Link to="/" className="error-page__link">
+        Back to home
+      </Link>
+    </div>
+  </section>;
 };
--- rs-react/src/pages/HomePage/HomePage.tsx
+++ rs-react/src/pages/HomePage/HomePage.tsx
@@ -6,25 +6,23 @@ import { mockData } from '../../common/data';
 import './HomePage.scss';
 
 export const HomePage = () => {
-  return (
-    <>
-      <Header checkHomeBtn={true} checkAboutBtn={false} />
-      <main className="main">
-        <div className="main__wrapper">
-          {mockData.map((el: IProduct) => (
-            <Card
-              key={el.id}
-              id={el.id}
-              title={el.title}
-              price={el.price}
-              image={el.image}
-              rating={el.rating}
-              category={el.category}
-              stock={el.stock}
-            />
-          ))}
-        </div>
-      </main>
-    </>
-  );
+  <>
+    <Header checkHomeBtn={true} checkAboutBtn={false} />
+    <main className="main">
+      <div className="main__wrapper">
+        {mockData.map((el: IProduct) => (
+          <Card
+            key={el.id}
+            id={el.id}
+            title={el.title}
+            price={el.price}
+            image={el.image}
+            rating={el.rating}
+            category={el.category}
+            stock={el.stock}
+          />
+        ))}
+      </div>
+    </main>
+  </>;
 };
