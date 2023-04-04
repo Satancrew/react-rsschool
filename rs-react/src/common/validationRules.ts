a7ccdd254e9208a7682fab4b8b22b5aaa202c30c
Vladimir Orlov <satancrew.true@gmail.com>
2023-04-04 12:42:55 +0300

    refactor: optimize validationRules

--- rs-react/src/common/validationRules.ts
+++ rs-react/src/common/validationRules.ts
@@ -1,11 +1,11 @@
 export const validationProductName = (prodName: string) => {
-  return prodName.length < 3 ? false : true;
+  return prodName.length > 3;
 };
 
 export const validationProductPriceStock = (prodPriceStock: string) => {
-  return prodPriceStock.length < 1 ? false : true;
+  return prodPriceStock.length > 1;
 };
 
 export const validationProductCategoryImage = (prodCategoryImage: string) => {
-  return prodCategoryImage.length === 0 ? false : true;
+  return prodCategoryImage.length !== 0;
 };
