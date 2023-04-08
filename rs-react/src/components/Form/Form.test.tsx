1009099ed5323748dd6e2a806defb4c6b31f6abf
Vladimir Orlov <satancrew.true@gmail.com>
2023-04-08 19:40:37 +0300

    fix: fix issue with import

--- rs-react/src/components/Form/Form.test.tsx
+++ rs-react/src/components/Form/Form.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { vitest } from 'vitest';
 import { Form } from './Form';
 import React from 'react';
