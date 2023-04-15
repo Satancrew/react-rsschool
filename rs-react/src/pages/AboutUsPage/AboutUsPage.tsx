82d7a3c5fa9c3713d5dfa61a03415a93e71eb06a
Vladimir Orlov <satancrew.true@gmail.com>
2023-04-15 15:24:09 +0300

    fix: delete console.log

--- rs-react/src/pages/AboutUsPage/AboutUsPage.tsx
+++ rs-react/src/pages/AboutUsPage/AboutUsPage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from '@/components/Header/Header';
 
 export const AboutUsPage = () => {
-  console.log('about us');
   return (
     <>
       <Header checkAboutBtn={true} checkHomeBtn={false} />
