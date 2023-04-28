46974113e104e54e1e211565f2c2362654e642c7
Vladimir Orlov <satancrew.true@gmail.com>
2023-04-28 23:36:09 +0300

    fix: fix some issue with App component and entry-server

--- rs-react/src/App.tsx
+++ rs-react/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage/HomePage';
 import { ErrorPage } from './pages/ErrorPage/ErrorPage';
@@ -7,18 +7,14 @@ import { FormPage } from './pages/FormPage/FormPage';
 import './App.scss';
 
 const App = () => {
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-  return mounted ? (
+  return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutUsPage />} />
       <Route path="/form" element={<FormPage />} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
-  ) : null;
+  );
 };
 
 export default App;
--- rs-react/src/entry-server.tsx
+++ rs-react/src/entry-server.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RenderToPipeableStreamOptions, renderToPipeableStream } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store';
@@ -7,9 +8,11 @@ import store from './store/store';
 export const render = (url: string, options: RenderToPipeableStreamOptions) => {
   return renderToPipeableStream(
     <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <StaticRouter location={url}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </StaticRouter>
     </React.StrictMode>,
     options
   );
